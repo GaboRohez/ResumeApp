a08e3ad67bb5a5ce09ff5f7e5d2b2c9f6870379f
Gabriel Rodríguez <gabow95k@gmail.com>
2019-11-04 23:04:47 -0600

    module experience

--- app/src/main/java/xyz/gabrielrohez/resumeapp/ui/experience/ExperienceFragment.java
+++ app/src/main/java/xyz/gabrielrohez/resumeapp/ui/experience/ExperienceFragment.java
@@ -2,20 +2,23 @@ package xyz.gabrielrohez.resumeapp.ui.experience;
 
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.os.Parcelable;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import java.util.ArrayList;
 import java.util.List;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import xyz.gabrielrohez.resumeapp.R;
+import xyz.gabrielrohez.resumeapp.adapters.ExperienceAdapter;
 import xyz.gabrielrohez.resumeapp.base.fragment.BasicFragment;
 import xyz.gabrielrohez.resumeapp.data.network.response.Experience;
 
@@ -23,6 +26,9 @@ import xyz.gabrielrohez.resumeapp.data.network.response.Experience;
  * A simple {@link Fragment} subclass.
  */
 public class ExperienceFragment extends BasicFragment {
+
+    @BindView(R.id.recyclerExperience)
+    RecyclerView recycler;
 
     private List<Experience> experience;
 
@@ -41,6 +47,7 @@ public class ExperienceFragment extends BasicFragment {
         View rootView = inflater.inflate(R.layout.fragment_experience, container, false);
         ButterKnife.bind(this, rootView);
         getParcelableData();
+        setExperienceRecyclerView();
         return rootView;
     }
 
@@ -51,4 +58,17 @@ public class ExperienceFragment extends BasicFragment {
         experience = getArguments().getParcelableArrayList("experience");
         Log.i("Experience", experience.toString());
     }
+
+    /**
+     * the information is shown in the recycler
+     */
+    private void setExperienceRecyclerView() {
+        ExperienceAdapter adapter = new ExperienceAdapter(experience);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        recycler.setLayoutManager(layoutManager);
+        recycler.setHasFixedSize(true);
+        recycler.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+    }
+
 }
